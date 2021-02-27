45eb5bec362636e8982d2425361ad2a39a318b28
Antoine Van Uffelen <antoine@mansio.be>
2021-02-27 14:11:18 +0100

    Linked data from db

--- src/main/java/be/thomasmore/chess/controllers/HomeController.java
+++ src/main/java/be/thomasmore/chess/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package be.thomasmore.chess.controllers;
 
 import be.thomasmore.chess.model.Opening;
+import be.thomasmore.chess.repository.OpeningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +11,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
-    Opening[] openings = {new Opening("1"), new Opening("2"), new Opening("3")};
-
+    @Autowired
+    private OpeningRepository openingRepository;
     @GetMapping({"/", "/home"})
     public String home() {
         return "home";
     }
     @GetMapping ({"/openingslist"})
     public String openingsList(Model model){
+        Iterable<Opening> openings = openingRepository.findAll();
         model.addAttribute("openings",openings);
         return "openingslist";
     }
     @GetMapping({"/openingdetails","/openingdetails/{variable}"})
     public String openingDetails(Model model, @PathVariable int variable){
-        model.addAttribute("opening",openings[variable]);
+        model.addAttribute("opening",(openingRepository.findById(variable).isPresent()) ? openingRepository.findById(variable).get() : null);
         return "openingdetails";
     }
 }
